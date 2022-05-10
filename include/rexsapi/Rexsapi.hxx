fad7582cca2ad21c6e7ae66a0e6d0e3f760cfc8b
Lars-Christian Fürstenberg <lcf@miztli.de>
2022-05-10 10:41:19 +0200

    Refactoring

--- include/rexsapi/Rexsapi.hxx
+++ include/rexsapi/Rexsapi.hxx
@@ -3,8 +3,9 @@
 #define REXSAPI_HXX
 
 #include <rexsapi/Defines.hxx>
+#include <rexsapi/ModelLoader.hxx>
 #include <rexsapi/Version.hxx>
-
+#include <rexsapi/XMLModelLoader.hxx>
 #include <rexsapi/database/FileResourceLoader.hxx>
 #include <rexsapi/database/ModelRegistry.hxx>
 #include <rexsapi/database/XMLModelLoader.hxx>
